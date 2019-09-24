88cb73c272fe186609db2fee3d294fcb18cbfea5
Akilesh Rao <akileshrao19@gmail.com>
2019-09-24 19:45:04 +0530

    Code Refacotring async await, folder structured

--- app.js
+++ app.js
@@ -1,71 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const grapqlHttp = require('express-graphql');
+const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
-const { buildSchema } = require('graphql');
-const Event = require('./models/event');
+const graphqlSchema = require('./graphql/schema/index');
+const graphqlResolvers = require('./graphql/resolvers/index');
+
 
 const app = express();
 
+
 app.use(bodyParser.json());
 
-app.use('/graphql', grapqlHttp({
-    schema: buildSchema(`
-        type Event {
-            _id : ID!
-            title : String!
-            description : String!
-            price : Float!
-            date : String!
-        }
-
-        input EventInput {
-            title : String!
-            description : String! 
-            price : Float!
-            date : String!
-        }
-
-        type RootQuery {
-            events : [Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput : EventInput) : Event
-        }
-
-        schema {
-            query : RootQuery 
-            mutation : RootMutation
-        }
-    `),
-    rootValue: {
-        events: () => {
-            return Event.find().then(events => {
-                return events.map(event => {
-                    return { ...event._doc }
-                })
-            }).catch(err => {
-                throw err;
-            })
-        },
-        createEvent: (args) => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
-            })
-            event.save().then(res => {
-                console.log(res);
-                return { ...res._doc };
-            }).catch(err => {
-                console.log(err);
-                throw err;
-            });
-            return event;
-        }
-    },
+app.use('/graphql', graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
     graphiql: true
 }))
 
