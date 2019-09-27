c4647ef98e3c0d10924b327ba59c532fe779edad
Akilesh Rao <akileshrao19@gmail.com>
2019-09-27 14:00:07 +0530

    Meh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from='/' to='/auth' exact />
-        <Route path='/auth' component={AuthPage}></Route>
-        <Route path='/events' component={EventsPage}></Route>
-        <Route path='/bookings' component={BookingsPage}></Route>
-      </Switch>
+      <MainNavigation />
+        <main>
+        <Switch>
+          <Redirect from='/' to='/auth' exact />
+          <Route path='/auth' component={AuthPage}></Route>
+          <Route path='/events' component={EventsPage}></Route>
+          <Route path='/bookings' component={BookingsPage}></Route>
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 }
