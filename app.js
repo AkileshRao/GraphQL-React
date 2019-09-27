1eaf732580efc93708d418118ce42863c17f4590
Akilesh Rao <akileshrao19@gmail.com>
2019-09-27 18:41:16 +0530

    Added Login and Signup

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const graphqlResolvers = require('./graphql/resolvers/index');
 const isAuth = require('./middleware/is-auth');
 
 const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS'){
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(isAuth);
 
 app.use(bodyParser.json());
@@ -21,7 +32,7 @@ app.use('/graphql', graphqlHttp({
 mongoose.connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-g35ev.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 ).then(() => {
-    app.listen(3000, () => {
+    app.listen(8000, () => {
         console.log("Connected to db");
     });
 }).catch(err => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <MainNavigation />
-        <main>
+        <main className='main-content'>
         <Switch>
           <Redirect from='/' to='/auth' exact />
           <Route path='/auth' component={AuthPage}></Route>
