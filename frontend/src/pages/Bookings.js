e5e640e29b1ab727728f8017955346af64015178
Akilesh Rao <akileshrao19@gmail.com>
2019-10-03 13:22:01 +0530

    Cancel booking

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AuthContext from '../context/auth-context';
 import Spinner from '../components/Spinner/Spinner';
+import BookingList from '../components/Bookings/BookingList/BookingList';
 
 class BookingsPage extends Component {
     static contextType = AuthContext;
@@ -12,6 +13,47 @@ class BookingsPage extends Component {
 
     componentDidMount() {
         this.fetchBookings();
+    };
+
+
+    deleteBookingHandler = bookingId => {
+        this.setState({ isLoading: true })
+        const requestBody = {
+            query: `
+                mutation {
+                    cancelBooking(bookingId : "${bookingId}"){
+                        _id
+                        title
+                    }
+                }
+            `,
+
+        };
+
+        const token = this.context.token;
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        }).then(res => {
+            if (res.status !== 200 && res.status !== 201) {
+                throw new Error('Failed');
+            }
+            return res.json();
+        }).then(resData => {
+            this.setState(prevState => {
+                const updatedBookings = prevState.bookings.filter(booking => {
+                    return booking._id !== bookingId;
+                });
+                return { bookings: updatedBookings, isLoading: false };
+            })
+        }).catch(err => {
+            this.setState({ isLoading: false })
+            console.log(err);
+        });
     };
 
     fetchBookings = () => {
@@ -60,13 +102,7 @@ class BookingsPage extends Component {
                 {
                     this.state.isLoading ?
                         (<Spinner />) : (
-                            <div>
-                                <ul>
-                                    {this.state.bookings.map(booking => {
-                                        return <li key={booking._id}>{booking.event.title} - {new Date(booking.createdAt).toLocaleDateString()}</li>
-                                    })}
-                                </ul>
-                            </div>
+                            <BookingList bookings={this.state.bookings} onDelete={this.deleteBookingHandler} />
                         )
                 }
 
